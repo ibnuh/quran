ebe65d43ca581626a88a9028bb2ec69c317e2a05
Muhammad Ibnuh <ibnuhdev@gmail.com>
2026-03-14 17:25:14 +0800

    Add all available reciters from qurancdn.com

--- src/data/reciters.js
+++ src/data/reciters.js
@@ -1,5 +1,6 @@
 export default [
   { id: 7, name: "Mishari Rashid al-Afasy" },
+  { id: 173, name: "Mishari Rashid al-Afasy (Alt)" },
   { id: 2, name: "AbdulBaset AbdulSamad (Murattal)" },
   { id: 1, name: "AbdulBaset AbdulSamad (Mujawwad)" },
   { id: 3, name: "Abdur-Rahman as-Sudais" },
@@ -8,6 +9,7 @@ export default [
   { id: 6, name: "Mahmoud Khalil Al-Husary" },
   { id: 12, name: "Mahmoud Khalil Al-Husary (Muallim)" },
   { id: 9, name: "Mohamed Siddiq al-Minshawi" },
+  { id: 168, name: "Mohamed Siddiq al-Minshawi (Kids Repeat)" },
   { id: 10, name: "Sa'ud ash-Shuraym" },
   { id: 97, name: "Yasser Ad-Dussary" },
   { id: 161, name: "Khalifah Al Tunaiji" }
