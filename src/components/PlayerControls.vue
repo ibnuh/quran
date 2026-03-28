097f66820d17f0ab9a5317d6f1a71932947a3c1e
Muhammad Ibnuh <ibnuhdev@gmail.com>
2026-03-28 21:01:14 +0800

    Fix repeat mode badge centering

--- src/components/PlayerControls.vue
+++ src/components/PlayerControls.vue
@@ -75,7 +75,7 @@ function selectSpeed(speed) {
         >
           <span v-if="store.repeatMode === 'verse'" class="relative inline-flex">
             <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M7 7h10v3l4-4-4-4v3H5v6h2V7zm10 10H7v-3l-4 4 4 4v-3h12v-6h-2v4z"/></svg>
-            <span class="absolute inset-0 flex items-center justify-center text-[7px] font-bold leading-none" style="padding-top: 1px">1</span>
+            <span class="absolute inset-0 flex items-center justify-center text-[7px] font-bold">1</span>
           </span>
           <svg v-else width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M7 7h10v3l4-4-4-4v3H5v6h2V7zm10 10H7v-3l-4 4 4 4v-3h12v-6h-2v4z"/></svg>
           <span class="hidden sm:inline text-[0.7rem]">{{ store.repeatMode === 'none' ? 'Repeat' : store.repeatMode === 'verse' ? 'Verse' : 'Surah' }}</span>
